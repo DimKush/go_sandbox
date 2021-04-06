659fe586d2d257466b47b8d3b0b04730d802b885
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-06 22:03:24 +0300

    01 | play with golang

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	randomized := rand.Intn(100) + 1
+
+	fmt.Println()
+
+	for {
+
+		fmt.Print("Enter a number from 1 to 100: ")
+		reader := bufio.NewReader(os.Stdin)
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Incorrect input %s", err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+		fmt.Print(input)
+
+		inputNum, err := strconv.Atoi(input)
+		if err != nil || inputNum > 100 || inputNum < 0 {
+			log.Fatal("Incorrect number")
+		}
+
+		if inputNum == randomized {
+			fmt.Println("Congratulations, you guessed a randomized number.", randomized)
+		} else {
+			fmt.Println("Sorry, you couldn't guess a randomized number : ", randomized)
+		}
+
+		fmt.Print("Continue? yes/no? : ")
+		reader = bufio.NewReader(os.Stdin)
+		input, err = reader.ReadString('\n')
+
+		input = strings.TrimSpace(input)
+		input = strings.ToLower(input)
+
+		if input == "no" {
+			break
+		} else if input == "yes" {
+			randomized = rand.Intn(100) + 1
+		}
+	}
+}
