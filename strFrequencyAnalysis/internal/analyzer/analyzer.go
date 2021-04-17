66ef5054f7054fc4156add2c0d8f7beb6ba4fba5
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-17 21:36:06 +0300

    03-sandbox | try to count words in string

--- strFrequencyAnalysis/internal/analyzer/analyzer.go
+++ strFrequencyAnalysis/internal/analyzer/analyzer.go
@@ -2,19 +2,58 @@ package analyzer
 
 import (
 	"fmt"
+	"sort"
 	"strings"
+	"unicode"
 )
 
-func cutStringOnWords(str string) {
+type wordCount struct {
+	word  string
+	count int
+}
+
+func clearWordsFromPunct(words []string) (clearWords []string) {
+	var tmpStr strings.Builder
+	for _, word := range words {
+
+		tmpStr.Reset()
+
+		word = strings.ToLower(word)
+
+		for _, r := range word {
+			if unicode.IsLetter(r) {
+				tmpStr.WriteRune(r)
+			}
+		}
+		clearWords = append(clearWords, tmpStr.String())
+	}
+	return
+}
+
+func cutStringOnWords(str string) []string {
 	str = strings.ToLower(str)
 
 	words := strings.Fields(str)
 
-	for _, val := range words {
-		fmt.Println(val)
-	}
+	return clearWordsFromPunct(words)
 }
 
 func Analyzer(str string) {
-	cutStringOnWords(str)
+	words := cutStringOnWords(str)
+	var sliceCountWords []wordCount
+	for _, wordColl := range words {
+		idx := sort.Search(len(sliceCountWords), func(i int) bool {
+			return sliceCountWords[i].word == wordColl
+		})
+		// if not found
+		if idx == len(sliceCountWords) {
+			sliceCountWords = append(sliceCountWords, wordCount{word: wordColl, count: 1})
+		} else {
+			sliceCountWords[idx].count += 1
+		}
+	}
+
+	for _, counted := range sliceCountWords {
+		fmt.Printf("\n%s = %d", counted.word, counted.count)
+	}
 }
