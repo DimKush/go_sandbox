9297fa7fa1b5ec0c04e3820d84e0442f35f333ba
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-20 00:16:22 +0300

    04-sandbox | create BidirNode

--- BidirList/internal/BidirNode/BidirNode.go
+++ BidirList/internal/BidirNode/BidirNode.go
@@ -9,3 +9,15 @@ type BidirNode struct {
 func ConstructNode(dataIn int) *BidirNode {
 	return &BidirNode{data: dataIn}
 }
+
+func (d *BidirNode) SetData(val int) {
+	d.data = val
+}
+
+func (d *BidirNode) SetNext(nxt *BidirNode) {
+	d.next = nxt.next
+}
+
+func (d *BidirNode) SetPrev(prv *BidirNode) {
+	d.prev = prv.prev
+}
