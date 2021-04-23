a6e6724b9c72c2d1305c5f84b3d4cc1a38797bad
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-23 23:33:30 +0300

    04-sandbox | done unit tests for BiDirList

--- BidirList/cmd/main_test.go
+++ BidirList/cmd/main_test.go
@@ -22,12 +22,13 @@ func TestPushBack(t *testing.T) {
 func TestPushFront(t *testing.T) {
 	testList := BidirList.BidirList{}
 	err := testList.PushBack(10)
+	err = testList.PushBack(20)
 
 	if err != nil {
 		t.Error(err)
 	}
 
-	if testList.First().GetData() != 10 {
+	if testList.Last().GetData() != 20 {
 		t.Error("Error in TestPushBack")
 	}
 }
@@ -46,5 +47,54 @@ func TestConstruct(t *testing.T) {
 		if dataTst[pos] != cur.GetData() {
 			t.Error("Error in ConstructList")
 		}
+	}
+}
+
+func TestListLen(t *testing.T) {
+	testList := BidirList.BidirList{}
+	err := testList.ConstructList([]interface{}{100, 10, 20, 30, 40})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if testList.Len() != 5 {
+		t.Errorf("Error in func BidirList.Len(): %d - expected; fact - %d", 5, testList.Len())
+	}
+}
+
+func TestRemove(t *testing.T) {
+	testList := BidirList.BidirList{}
+
+	err := testList.ConstructList([]interface{}{100, 10, 20, 30, 40})
+	if err != nil {
+		t.Error(err)
+	}
+
+	rmv := testList.First()
+	rmv = rmv.Next().Next()
+	testList.Remove(rmv)
+
+	dataTst := []interface{}{100, 10, 30, 40, 50}
+	var pos int
+
+	for cur := testList.First(); cur == testList.Last(); cur = cur.Next() {
+		if dataTst[pos] != cur.GetData() {
+			t.Error("Error in Remove()")
+		}
+	}
+}
+
+func TestGetData(t *testing.T) {
+	testList := BidirList.BidirList{}
+
+	err := testList.ConstructList([]interface{}{100, 10, 20, 30, 40})
+	if err != nil {
+		t.Error(err)
+	}
+
+	curData := testList.First().GetData()
+	if curData != 100 {
+		t.Errorf("Error in func BidirList.Len(): %d - expected; fact - %d", 100, curData)
 	}
 }
