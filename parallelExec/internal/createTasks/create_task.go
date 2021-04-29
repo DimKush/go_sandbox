f4c5a897a2e840748165c07fb4fc698f372c33b1
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-04-29 23:02:21 +0300

    05-sandbox | return struct of status

--- parallelExec/internal/createTasks/create_task.go
+++ parallelExec/internal/createTasks/create_task.go
@@ -1,6 +1,8 @@
 package create_task
 
 import (
+	"errors"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -12,20 +14,34 @@ type Tracker_unit struct {
 	Operations       int
 }
 
-func CreateTask(TasksCount int) []func(v int, ch chan<- Tracker_unit, wg *sync.WaitGroup) {
-	var taskSlice []func(v int, ch chan<- Tracker_unit, wg *sync.WaitGroup)
+type Task_Unit struct {
+	unit Tracker_unit
+	err  error
+}
+
+func CreateTask(TasksCount int) []func(v int, ch chan<- Task_Unit, wg *sync.WaitGroup) {
+	var taskSlice []func(v int, ch chan<- Task_Unit, wg *sync.WaitGroup)
 
 	for i := 1; i <= TasksCount; i++ {
 
-		taskSlice = append(taskSlice, func(v int, ch chan<- Tracker_unit, wg *sync.WaitGroup) {
+		taskSlice = append(taskSlice, func(v int, ch chan<- Task_Unit, wg *sync.WaitGroup) {
 			defer wg.Done()
 			start := time.Now()
 
 			oprs := 0
 			resVal := countFib(v, &oprs)
 
-			ch <- Tracker_unit{FibVal: v, ExecutionResTime: time.Since(start), FinResult: resVal, Operations: oprs}
+			// if the countFib func count more that 10 seconds
+			if time.Since(start) > time.Duration(8*time.Second) {
+				str := "To slow for count value : " + strconv.Itoa(v)
+				ch <- Task_Unit{unit: Tracker_unit{}, err: errors.New(str)}
+			}
 
+			ch <- Task_Unit{unit: Tracker_unit{FibVal: v,
+				ExecutionResTime: time.Since(start),
+				FinResult:        resVal,
+				Operations:       oprs},
+				err: nil}
 		})
 	}
 	return taskSlice
