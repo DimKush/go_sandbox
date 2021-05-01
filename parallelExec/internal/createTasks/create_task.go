917e2e72231ce7a4996304e45243349aa74f98af
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-05-01 21:45:47 +0300

    05-sandbox | need unit tests and fix bug with an output

--- parallelExec/internal/createTasks/create_task.go
+++ parallelExec/internal/createTasks/create_task.go
@@ -2,7 +2,6 @@ package create_task
 
 import (
 	"errors"
-	"strconv"
 	"time"
 )
 
@@ -14,10 +13,10 @@ type Tracker_unit struct {
 	Status           error
 }
 
-func CreateTask(TasksCount int) []func(v int, ch chan<- Tracker_unit) {
+func CreateTasks(TasksCount int) []func(v int, ch chan<- Tracker_unit) {
 	var taskSlice []func(v int, ch chan<- Tracker_unit)
 
-	for i := 1; i <= TasksCount; i++ {
+	for i := 0; i <= TasksCount; i++ {
 
 		taskSlice = append(taskSlice, func(v int, ch chan<- Tracker_unit) {
 			start := time.Now()
@@ -27,18 +26,41 @@ func CreateTask(TasksCount int) []func(v int, ch chan<- Tracker_unit) {
 
 			// if the countFib func count more that 8 seconds
 			if time.Since(start) > time.Duration(8*time.Second) {
-				str := "To slow for count value : " + strconv.Itoa(v)
-				ch <- Tracker_unit{Status: errors.New(str)}
+				str := "To slow for count value : " + time.Since(start).String()
+				ch <- Tracker_unit{FibVal: v, Status: errors.New(str)}
+				return
 			}
 
 			ch <- Tracker_unit{FibVal: v,
 				ExecutionResTime: time.Since(start),
 				FinResult:        resVal,
-				Operations:       oprs,
-				Status:           nil}
+				Operations:       oprs}
 		})
 	}
 	return taskSlice
+}
+
+func CreateTask() func(v int, ch chan<- Tracker_unit) {
+	task := func(v int, ch chan<- Tracker_unit) {
+		start := time.Now()
+
+		oprs := 0
+		resVal := countFib(v, &oprs)
+
+		// if the countFib func count more that 8 seconds
+		if time.Since(start) > time.Duration(8*time.Second) {
+			str := "To slow for count value. time is : " + time.Since(start).String()
+			ch <- Tracker_unit{FibVal: v, Status: errors.New(str)}
+			return
+		}
+
+		ch <- Tracker_unit{FibVal: v,
+			ExecutionResTime: time.Since(start),
+			FinResult:        resVal,
+			Operations:       oprs}
+	}
+
+	return task
 }
 
 func countFib(val int, operations *int) int {
